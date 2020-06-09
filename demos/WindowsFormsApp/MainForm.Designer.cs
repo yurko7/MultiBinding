447a045b18892e3a74031b7b5955f800fe1ef595
Yuriy Kushnir <yurko7@hotmail.com>
2020-06-09 21:41:24 +0300

    Add simple demo project.

---
+++ demos/WindowsFormsApp/MainForm.Designer.cs
@@ -0,0 +1,186 @@
+﻿namespace WindowsFormsApp
+{
+    partial class MainForm
+    {
+        /// <summary>
+        ///  Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        ///  Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        ///  Required method for Designer support - do not modify
+        ///  the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.checkBox2 = new System.Windows.Forms.CheckBox();
+            this.checkBox3 = new System.Windows.Forms.CheckBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox = new System.Windows.Forms.PictureBox();
+            this.labelR = new System.Windows.Forms.Label();
+            this.textBoxR = new System.Windows.Forms.TextBox();
+            this.labelG = new System.Windows.Forms.Label();
+            this.textBoxG = new System.Windows.Forms.TextBox();
+            this.labelB = new System.Windows.Forms.Label();
+            this.textBoxB = new System.Windows.Forms.TextBox();
+            this.colorDialog = new System.Windows.Forms.ColorDialog();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.Location = new System.Drawing.Point(12, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(83, 19);
+            this.checkBox1.TabIndex = 0;
+            this.checkBox1.Text = "checkBox1";
+            this.checkBox1.UseVisualStyleBackColor = true;
+            // 
+            // checkBox2
+            // 
+            this.checkBox2.AutoSize = true;
+            this.checkBox2.Location = new System.Drawing.Point(12, 37);
+            this.checkBox2.Name = "checkBox2";
+            this.checkBox2.Size = new System.Drawing.Size(83, 19);
+            this.checkBox2.TabIndex = 1;
+            this.checkBox2.Text = "checkBox2";
+            this.checkBox2.UseVisualStyleBackColor = true;
+            // 
+            // checkBox3
+            // 
+            this.checkBox3.AutoSize = true;
+            this.checkBox3.Location = new System.Drawing.Point(12, 62);
+            this.checkBox3.Name = "checkBox3";
+            this.checkBox3.Size = new System.Drawing.Size(83, 19);
+            this.checkBox3.TabIndex = 2;
+            this.checkBox3.Text = "checkBox3";
+            this.checkBox3.UseVisualStyleBackColor = true;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(101, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(75, 69);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "button1";
+            this.button1.UseVisualStyleBackColor = true;
+            // 
+            // pictureBox
+            // 
+            this.pictureBox.BackColor = System.Drawing.Color.White;
+            this.pictureBox.Location = new System.Drawing.Point(218, 12);
+            this.pictureBox.Name = "pictureBox";
+            this.pictureBox.Size = new System.Drawing.Size(81, 81);
+            this.pictureBox.TabIndex = 4;
+            this.pictureBox.TabStop = false;
+            this.pictureBox.Click += new System.EventHandler(this.pictureBox_Click);
+            // 
+            // labelR
+            // 
+            this.labelR.AutoSize = true;
+            this.labelR.Location = new System.Drawing.Point(307, 15);
+            this.labelR.Name = "labelR";
+            this.labelR.Size = new System.Drawing.Size(17, 15);
+            this.labelR.TabIndex = 4;
+            this.labelR.Text = "R:";
+            // 
+            // textBoxR
+            // 
+            this.textBoxR.Location = new System.Drawing.Point(330, 12);
+            this.textBoxR.Name = "textBoxR";
+            this.textBoxR.Size = new System.Drawing.Size(52, 23);
+            this.textBoxR.TabIndex = 5;
+            this.textBoxR.Text = "0";
+            // 
+            // labelG
+            // 
+            this.labelG.AutoSize = true;
+            this.labelG.Location = new System.Drawing.Point(307, 44);
+            this.labelG.Name = "labelG";
+            this.labelG.Size = new System.Drawing.Size(18, 15);
+            this.labelG.TabIndex = 6;
+            this.labelG.Text = "G:";
+            // 
+            // textBoxG
+            // 
+            this.textBoxG.Location = new System.Drawing.Point(330, 41);
+            this.textBoxG.Name = "textBoxG";
+            this.textBoxG.Size = new System.Drawing.Size(52, 23);
+            this.textBoxG.TabIndex = 7;
+            this.textBoxG.Text = "0";
+            // 
+            // labelB
+            // 
+            this.labelB.AutoSize = true;
+            this.labelB.Location = new System.Drawing.Point(307, 73);
+            this.labelB.Name = "labelB";
+            this.labelB.Size = new System.Drawing.Size(17, 15);
+            this.labelB.TabIndex = 8;
+            this.labelB.Text = "B:";
+            // 
+            // textBoxB
+            // 
+            this.textBoxB.Location = new System.Drawing.Point(330, 70);
+            this.textBoxB.Name = "textBoxB";
+            this.textBoxB.Size = new System.Drawing.Size(52, 23);
+            this.textBoxB.TabIndex = 9;
+            this.textBoxB.Text = "0";
+            // 
+            // MainForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(7F, 15F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(420, 213);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.checkBox2);
+            this.Controls.Add(this.checkBox3);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.pictureBox);
+            this.Controls.Add(this.labelR);
+            this.Controls.Add(this.textBoxR);
+            this.Controls.Add(this.labelG);
+            this.Controls.Add(this.textBoxG);
+            this.Controls.Add(this.labelB);
+            this.Controls.Add(this.textBoxB);
+            this.Name = "MainForm";
+            this.Text = "MultiBinding Demo";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.CheckBox checkBox2;
+        private System.Windows.Forms.CheckBox checkBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox;
+        private System.Windows.Forms.Label labelR;
+        private System.Windows.Forms.TextBox textBoxR;
+        private System.Windows.Forms.Label labelG;
+        private System.Windows.Forms.TextBox textBoxG;
+        private System.Windows.Forms.Label labelB;
+        private System.Windows.Forms.TextBox textBoxB;
+        private System.Windows.Forms.ColorDialog colorDialog;
+    }
+}
+
---
+++ demos/WindowsFormsApp/MainForm.cs
@@ -0,0 +1,59 @@
+﻿using System;
+using System.Drawing;
+using System.Linq;
+using System.Windows.Forms;
+using YuKu.Windows.Forms;
+
+namespace WindowsFormsApp
+{
+    public partial class MainForm : Form
+    {
+        public MainForm()
+        {
+            InitializeComponent();
+
+            InitializeButtonBinding();
+            InitializeColorBinding();
+        }
+
+        private void InitializeButtonBinding()
+        {
+            var multiBinding = new MultiBinding(nameof(Button.Enabled), dataSourceUpdateMode: DataSourceUpdateMode.Never);
+            multiBinding.AddBinding(checkBox1, nameof(CheckBox.Checked));
+            multiBinding.AddBinding(checkBox2, nameof(CheckBox.Checked));
+            multiBinding.AddBinding(checkBox3, nameof(CheckBox.Checked));
+            multiBinding.Format += (sender, args) => args.Value = ((Object[]) args.Value).Cast<Boolean?>().All(value => value ?? false);
+            multiBinding.AddTo(button1);
+        }
+
+        private void InitializeColorBinding()
+        {
+            var multiBinding = new MultiBinding(nameof(PictureBox.BackColor), dataSourceUpdateMode: DataSourceUpdateMode.OnPropertyChanged);
+            multiBinding.AddBinding(textBoxR, nameof(TextBox.Text), dataSourceUpdateMode: DataSourceUpdateMode.OnPropertyChanged);
+            multiBinding.AddBinding(textBoxG, nameof(TextBox.Text), dataSourceUpdateMode: DataSourceUpdateMode.OnPropertyChanged);
+            multiBinding.AddBinding(textBoxB, nameof(TextBox.Text), dataSourceUpdateMode: DataSourceUpdateMode.OnPropertyChanged);
+            multiBinding.Format += (sender, args) =>
+            {
+                var values = (Object[]) args.Value;
+                Int32.TryParse((String?) values[0], out Int32 r);
+                Int32.TryParse((String?) values[1], out Int32 g);
+                Int32.TryParse((String?) values[2], out Int32 b);
+                args.Value = Color.FromArgb(r, g, b);
+            };
+            multiBinding.Parse += (sender, args) =>
+            {
+                var color = (Color) args.Value;
+                args.Value = new Object[] {color.R.ToString(), color.G.ToString(), color.B.ToString()};
+            };
+            multiBinding.AddTo(pictureBox);
+        }
+
+        private void pictureBox_Click(Object sender, EventArgs e)
+        {
+            if (colorDialog.ShowDialog(this) == DialogResult.OK)
+            {
+                pictureBox.BackColor = colorDialog.Color;
+            }
+        }
+    }
+}
